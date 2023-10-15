33b9503cce4ccffe2a190337306d5a3e2c71bb2f
Jeziel Almeida <jeziel.almeida16@gmail.com>
2023-10-15 16:12:01 -0300

    refactor: delete src folder and jwt auth

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import admin from 'firebase-admin';
+
+const app = express();
+
+admin.initializeApp({
+    credential: admin.credential.cert("serviceAccountKey.json")
+});
+
+app.get("/transactions", async (request, response) => {
+    
+    const jwt = request.headers.authorization;
+    if(!jwt) {
+        response.status(401).json({ message: "Usuário não autorizado"})
+        return;
+    }
+
+    // let decodedIdToken = "";
+
+    // try {
+    //     decodedIdToken = await admin.auth().verifyIdToken(jwt, true);
+    // } catch(e) {
+    //     response.status(401).json({ message: "Usuário não autorizado"})
+    //     return;
+    // }
+
+    admin.firestore()
+        .collection('transactions')
+        //.where('user.uid', '==', decodedIdToken.sub)
+        .where('user.uid', '==', jwt)
+        .orderBy('date', 'desc')
+        .get()
+        .then(snapshot => {
+            const transactions = snapshot.docs.map(doc => ({
+                ...doc.data(),
+                uid: doc.id
+            }))
+            response.json(transactions)
+        })
+})
+
+app.listen(3000, () => console.log("API Rest iniciada em http://localhost:3000"))
