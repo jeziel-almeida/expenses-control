4722a4e328b04db890552231b377a59623ef381d
Jeziel Almeida <jeziel.almeida16@gmail.com>
2023-10-15 12:39:23 -0300

    feat: add get transactions

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import admin from 'firebase-admin';
+
+const app = express();
+
+admin.initializeApp({
+    credential: admin.credential.cert("serviceAccountKey.json")
+});
+
+app.get("/transactions", (request, response) => {
+    console.log('GET transaction');
+    admin.firestore()
+        .collection('transactions')
+        .get()
+        .then(snapshot => {
+            const transactions = snapshot.docs.map(doc => ({
+                ...doc.data(),
+                uid: doc.id
+            }))
+            response.json(transactions)
+        })
+})
+
+app.listen(3000, () => console.log("API Rest iniciada em http://localhost:3000"))
