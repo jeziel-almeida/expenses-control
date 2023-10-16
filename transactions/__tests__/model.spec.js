b1bd588113d90cd5aa477970dbae4d8c7678ed39
Jeziel Almeida <jeziel.almeida16@gmail.com>
2023-10-16 12:47:37 -0300

    feat: add model transactions unit tests

--- transactions/__tests__/model.spec.js
+++ transactions/__tests__/model.spec.js
@@ -1,47 +1,99 @@
+import { TransactionNotFoundError } from "../erros/transaction-not-found.error.js";
+import { TransactionUidNotInformedError } from "../erros/transaction-uid-not-informed.error.js";
 import { UserNotInformedError } from "../erros/user-not-informed.error.js";
 import { Transaction } from "../model.js";
 
 describe("Transaction Model", () => {
 
-    const transactionRepositoryMock = {
-        findByUserUid: () => Promise.resolve([
-            { uid: "transaction1" }, { uid: "transaction2" }
-        ])
-    }
-
     describe("given find user by uid", () => {
 
         test("when user is not informed, then return error 400", async () => {
-    
+
             const model = new Transaction();
-    
+
             const response = model.findByUser();
-    
+
             await expect(response).rejects.toBeInstanceOf(UserNotInformedError);
         })
-    
+
         test("when user uid is not informed, then return error 400", async () => {
-    
+
             const model = new Transaction();
             model.user = {};
-    
+
             const response = model.findByUser();
-    
+
             await expect(response).rejects.toBeInstanceOf(UserNotInformedError);
         })
-    
+
         test("when user is informed, then return transactions", async () => {
-    
+
             const model = new Transaction(transactionRepositoryMock);
-    
+
             model.user = { uid: "anyUserUid" };
-    
+
             const response = model.findByUser();
-    
+
             await expect(response).resolves.toEqual([
                 { uid: "transaction1" }, { uid: "transaction2" }
             ]);
         })
+
+        const transactionRepositoryMock = {
+            findByUserUid: () => Promise.resolve([
+                { uid: "transaction1" }, { uid: "transaction2" }
+            ]),
+        }
+    })
+
+    describe("given find transactions by uid", () => {
+
+        test("when uid present, then return transaction", async () => {
+
+            const model = new Transaction({
+                findByUid: () => Promise.resolve(createTransaction())
+            });
+
+            model.uid = 1;
+
+            await model.findByUid();
+
+            expect(model).toEqual(createTransaction())
+        })
+
+        test("when uid not present, then return error 400", async () => {
+
+            const model = new Transaction();
+
+            await expect(model.findByUid()).rejects.toBeInstanceOf(TransactionUidNotInformedError);
+        })
+
+        test("when transaction not found, then return error 404", async () => {
+
+            const model = new Transaction({
+                findByUid: () => Promise.resolve(null)
+            });
+            model.uid = 9;
+
+            await expect(model.findByUid()).rejects.toBeInstanceOf(TransactionNotFoundError);
+        })
+
+        function createTransaction() {
+            const transaction = new Transaction();
+            transaction.uid = 1;
+            transaction.date = "anyDate";
+            transaction.description = "anyDescription";
+            transaction.money = {
+                currency: "anyCurrency",
+                value: 10
+            }
+            transaction.transactionType = "Supermercado";
+            transaction.type = "expense";
+            transaction.user = {
+                uid: "anyUserUid"
+            }
+            return transaction;
+        }
     })
 
 })
--- transactions/model.js
+++ transactions/model.js
@@ -1,8 +1,11 @@
+import { TransactionNotFoundError } from "./erros/transaction-not-found.error.js";
+import { TransactionUidNotInformedError } from "./erros/transaction-uid-not-informed.error.js";
 import { UserNotInformedError } from "./erros/user-not-informed.error.js";
 import { TransactionRepository } from "./repository.js";
 
 export class Transaction {
 
+    uid;
     date;
     description;
     money;
@@ -23,5 +26,24 @@ export class Transaction {
         }
 
         return this.#repository.findByUserUid(this.user.uid);
+    }
+    
+    findByUid() {
+
+        if(!this.uid) {
+            return Promise.reject(new TransactionUidNotInformedError());
+        }
+
+        this.#repository.findByUid(this.uid).then(transactionDb => {
+            if (!transactionDb) {
+                return Promise.reject(new TransactionNotFoundError());
+            }
+            this.date = transactionDb.date;
+            this.description = transactionDb.description;
+            this.money = transactionDb.money
+            this.transactionType = transactionDb.transactionType;
+            this.type = transactionDb.type;
+            this.user = transactionDb.user;
+        })
     }
 }
