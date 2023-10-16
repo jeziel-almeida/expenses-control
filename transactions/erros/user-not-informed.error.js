01902b3d5388adaa879d16af9349333274487900
Jeziel Almeida <jeziel.almeida16@gmail.com>
2023-10-15 23:18:40 -0300

    feat: add model unit tests

---
+++ transactions/erros/user-not-informed.error.js
@@ -0,0 +1,8 @@
+export class UserNotInformedError extends Error {
+
+    constructor() {
+        super("Usuário não informado");
+        this.name = "user-not-informed";
+        this.code = 400;
+    }
+}
--- transactions/model.js
+++ transactions/model.js
@@ -1,5 +1,5 @@
+import { UserNotInformedError } from "./erros/user-not-informed.error.js";
 import { TransactionRepository } from "./repository.js";
-
 
 export class Transaction {
 
@@ -12,17 +12,14 @@ export class Transaction {
 
     #repository;
 
-    constructor() {
-        this.#repository = new TransactionRepository();
+    constructor(transactionRepository) {
+        this.#repository = transactionRepository || new TransactionRepository();
     }
 
     findByUser() {
 
         if(!this.user?.uid) {
-            return Promise.reject({
-                code: 400,
-                message: "Usuário não informado."
-            })
+            return Promise.reject(new UserNotInformedError())
         }
 
         return this.#repository.findByUserUid(this.user.uid);
