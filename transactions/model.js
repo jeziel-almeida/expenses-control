0f0cbebc48b2bece5eee6b90da679f5d1bc1d418
Jeziel Almeida <jeziel.almeida16@gmail.com>
2023-10-18 11:56:26 -0300

    feat: add model update transaction unit tests

--- transactions/model.js
+++ transactions/model.js
@@ -64,4 +64,19 @@ export class Transaction {
             this.uid = response.uid;
         })
     }
+
+    update(params) {
+
+        return this.findByUid(this.uid).then(() => {
+            
+            this.date = params.date;
+            this.description = params.description;
+            this.money = params.money;
+            this.transactionType = params.transactionType;
+            this.type = params.type;
+            this.user = params.user;
+    
+            return this.#repository.update(this);
+        })
+    }
 }
